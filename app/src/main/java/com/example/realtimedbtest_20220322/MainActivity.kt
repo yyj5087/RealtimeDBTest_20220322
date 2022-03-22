806a3ea102e38958c003119d644ce6089aaafc4e
win10 <naki5087@naver.com>
2022-03-22 10:08:08 +0900

    제일 기본 형태의 데이터 기록 체험  - DB 연결  - 항목명 설정 (test)  - 항목에 실제 값 기록 (Hello world)

--- app/src/main/java/com/example/realtimedbtest_20220322/MainActivity.kt
+++ app/src/main/java/com/example/realtimedbtest_20220322/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.realtimedbtest_20220322
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +18,16 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        DB 연결 -> 값 기록 연습.
+
+        val db = FirebaseDatabase.getInstance("https://realtimedbtest-20220322-a06ee-default-rtdb.asia-southeast1.firebasedatabase.app/") // 싱가폴 DB주소 대입
+
+//        DB의 하위 항목 (Reference) 설정
+        val testRef = db.getReference("test")
+
+//        test 항목에, "Hello world!!" 기록
+        testRef.setValue("Hello world!!")
 
     }
 }
