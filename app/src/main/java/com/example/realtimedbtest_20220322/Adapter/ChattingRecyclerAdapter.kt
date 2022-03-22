653f959d09e4c9e8293ff21cffc6a57aa6609b22
win10 <naki5087@naver.com>
2022-03-22 11:13:41 +0900

    채팅 어댑터 추가

---
+++ app/src/main/java/com/example/realtimedbtest_20220322/Adapter/ChattingRecyclerAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.realtimedbtest_20220322.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.realtimedbtest_20220322.R
+import com.example.realtimedbtest_20220322.datas.ChattingData
+
+class ChattingRecyclerAdapter(
+    val mContext: Context,
+    val mList: List<ChattingData>
+) : RecyclerView.Adapter<ChattingRecyclerAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+
+
+        fun bing(data: ChattingData) {
+
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val view =
+            LayoutInflater.from(mContext).inflate(R.layout.chatting_list_item, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val data = mList[position]
+        holder.bing(data)
+    }
+
+    override fun getItemCount() = mList.size
+
+}
