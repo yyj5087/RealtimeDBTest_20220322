02398d364085e3ef5cbd9dc9a051bc80b03fa474
win10 <naki5087@naver.com>
2022-03-22 09:45:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/realtimedbtest_20220322/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.realtimedbtest_20220322
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
