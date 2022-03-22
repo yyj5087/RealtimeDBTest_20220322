85450b7b461e256c98dba19faf017c95dfb3b495
win10 <naki5087@naver.com>
2022-03-22 11:32:32 +0900

    DB 변경시 => 마지막으로 추가된 사항을, ChattingData로 변환 / 목록에 추가  - 변경 항목 전체 중, 원하는 항목을 찾아가서 활용

--- app/src/main/java/com/example/realtimedbtest_20220322/MainActivity.kt
+++ app/src/main/java/com/example/realtimedbtest_20220322/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.realtimedbtest_20220322
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.realtimedbtest_20220322.datas.ChattingData
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -13,6 +14,8 @@ import java.util.*
 class MainActivity : BaseActivity() {
 
     var messageCount = 0L // DB에 저장된 채팅 갯수를 담을 변수. Long 타입으로 저장.
+
+    val mChattingList = ArrayList<ChattingData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +36,14 @@ class MainActivity : BaseActivity() {
 //                snapshot변수 : 현재 변경된 상태 => 자녀가 몇개인지 추출.
 
                 messageCount = snapshot.childrenCount
+
+//                snapshot => 마지막 자녀 (최신 채팅 메세지) 추출 => ChattingData로 변환 + 목록에 추가
+                mChattingList.add(
+                    ChattingData(
+                    snapshot.children.last().child("content").value.toString(),
+                    snapshot.children.last().child("createdAt").value.toString()
+                )
+                )
 
 
 
